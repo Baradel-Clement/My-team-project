e799919763b7c6abd9d340808bd253003a9d8b8e
Clément Baradel <baradelclement@gmail.com>
2020-10-30 00:40:41 +0100

    .

--- srcs/1 - Scripts/app.js
+++ srcs/1 - Scripts/app.js
@@ -31,31 +31,33 @@ const inputs = document.querySelectorAll('input');
 const form = document.querySelector('#contact-form');
 const textareas = document.querySelectorAll('textarea');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    inputs.forEach(input => {
-        if (input.value == '' || input.value == null) {
-            input.nextElementSibling.classList.add('active');
-            input.parentElement.classList.remove('valid')
-            input.parentElement.classList.add('nonvalid')
-        } else {
-            input.nextElementSibling.classList.remove('active');
-            input.parentElement.classList.remove('nonvalid')
-            input.parentElement.classList.add('valid')
-        }
+if (form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        inputs.forEach(input => {
+            if (input.value == '' || input.value == null) {
+                input.nextElementSibling.classList.add('active');
+                input.parentElement.classList.remove('valid')
+                input.parentElement.classList.add('nonvalid')
+            } else {
+                input.nextElementSibling.classList.remove('active');
+                input.parentElement.classList.remove('nonvalid')
+                input.parentElement.classList.add('valid')
+            }
+        })
+        textareas.forEach(textarea => {
+            if (textarea.value == '' || textarea.value == null) {
+                textarea.nextElementSibling.classList.add('active');
+                textarea.parentElement.classList.remove('valid')
+                textarea.parentElement.classList.add('nonvalid')
+            } else {
+                textarea.nextElementSibling.classList.remove('active');
+                textarea.parentElement.classList.remove('nonvalid')
+                textarea.parentElement.classList.add('valid')
+            }
+        })
     })
-    textareas.forEach(textarea => {
-        if (textarea.value == '' || textarea.value == null) {
-            textarea.nextElementSibling.classList.add('active');
-            textarea.parentElement.classList.remove('valid')
-            textarea.parentElement.classList.add('nonvalid')
-        } else {
-            textarea.nextElementSibling.classList.remove('active');
-            textarea.parentElement.classList.remove('nonvalid')
-            textarea.parentElement.classList.add('valid')
-        }
-    })
-})
+}
 
 // Form contact start //
 
